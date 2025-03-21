0a3c1d080e76bae401f65b3dc900803bdf610718
Denis Neves <nevesdenis35@gmail.com>
2025-03-21 11:50:26 -0300

    cria exemplo5

---
+++ exemplo5/index.js
@@ -0,0 +1,11 @@
+console.log("Sextou");
+console.log("Gerenciando pacotes com NPM");
+
+const nome = "Kbc";
+const idade = 20;
+
+if (idade >= 18) {
+  console.log(`O ${nome} tem ${idade} anos. É maior de idade`);
+} else {
+  console.log(`O ${nome} tem ${idade} anos. É menor de idade`);
+}
